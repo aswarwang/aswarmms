8234c72bc86d466d1481c8241ca9f2b6b99dd95d
aswarwang <47450571+aswarwang@users.noreply.github.com>
2019-02-08 21:49:31 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas2</title>
+</head>
+<body>
+	<table>
+		<tr>
+			<td>Nama</td>
+			<td> : </td>
+			<td>Aswar</td>
+		</tr>
+		<tr>
+			<td>Nim</td>
+			<td> : </td>
+			<td>215280055</td>
+		</tr>
+		<tr><td>Kelas</td>
+			<td> : </td>
+			<td>B</td>
+		</tr>
+		<tr>
+			<td>Jurusan</td>
+			<td> : </td>
+			<td>Informatika</td>
+		</tr>
+	</table>
+</body>
+</html>
